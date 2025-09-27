4eb2db3369f52704e89fdf24c377731bd3dcf594
Shakhawat77 <shoadshakhawat7@gmail.com>
2025-09-27 20:07:26 +0600

    Connectig live server

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Count from "./component/Navbar/Count";
 import Footer from "./component/Footer";
 import 'react-toastify/dist/ReactToastify.css';
 
+
 const fetchCard = async () => {
   const res = await fetch("/ticket.json");
   return res.json();
